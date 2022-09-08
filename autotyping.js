8ef58310221ca83bce0fcbff439a5b9950064e96
Virdes <virdes_barbosa@hotmail.com>
2022-09-08 13:35:13 -0300

    alterando alguns dados portfólio

--- autotyping.js
+++ autotyping.js
@@ -119,7 +119,7 @@ var AutoTyping = (function (e) {
 ]).default;
 
 
-const exampleText = ['JavaScript', 'React', 'NodeJs', 'Next Js'];
+const exampleText = ['JavaScript', 'ReactJs', 'NodeJs', 'NextJs'];
 const exampleTyping = new AutoTyping('#text', exampleText, {
     typeSpeed: 100,
     deleteSpeed: 100,
